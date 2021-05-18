ebfe0ccc11101d5408b44d00265f49295337d28f
Utkarsh Moholkar <67200103+UtkarshMoholkar@users.noreply.github.com>
2021-05-18 12:32:24 +0530

    Update Scrapper.java

--- Scrapper/src/Scrapper.java
+++ Scrapper/src/Scrapper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public  class Scrapper
 {
-    public static void linkParser(Elements links)
+    public static void linkParser(Elements links) //Helper function to extract links
     {
         for (Element link: links)
         {
@@ -21,14 +21,11 @@ public  class Scrapper
     {
         String url = "https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020";
 
-
-
-
-
         try
         {
-            Document document = Jsoup.connect(url).get();
-//            System.out.println(document.outerHtml());
+            Document document = Jsoup.connect(url).get(); //Connection to the URL
+//            System.out.println(document.outerHtml()); This line prints whole HTML
+            //Code to extract data using CSS selectors
             System.out.println(document.select(".lbl-licitacao:nth-child(1)").text());
             System.out.println(document.select(".lbl-licitacao:nth-child(13)").text());
             System.out.println(document.select(".lbl-licitacao:nth-child(19)").text());
@@ -36,27 +33,17 @@ public  class Scrapper
             System.out.println(document.select("p:nth-child(6)").text());
 //            System.out.println(document.select("tr:nth-child(1) a[href]").get(1));
 
+
+            //            Code to extract links using CSS selectors
             Elements links1 = document.select("tr:nth-child(1) a[href]");
             Elements links2 = document.select("tr:nth-child(2) a[href]");
             Elements links3 = document.select("tr:nth-child(3) a[href]");
             Elements links4 = document.select("tr:nth-child(4) a[href]");
-//
+
             linkParser(links1);
             linkParser(links2);
             linkParser(links3);
             linkParser(links4);
-
-//            for (Element link: links)
-//            {
-//                if (link.attr("href").startsWith("http"))
-//                {
-//                    System.out.println(link.attr("href"));
-//                }
-//
-////                System.out.println("\nlink: " + link.attr("href"));
-////                System.out.println("text:" + link.text());
-//            }
-
 
         }
         catch (IOException e)
@@ -67,7 +54,6 @@ public  class Scrapper
     }
 
 }
-
 
 //    CSS Selectors
 // .lbl-licitacao:nth-child(1)
